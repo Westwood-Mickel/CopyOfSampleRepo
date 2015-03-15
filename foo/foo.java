7503d7d0cacc37345790f220f33f03a002df77bc
TX101010 <github@littlebox.net>
2015-03-15 01:40:23 -0500

    Revert 47af1fa..512647f
    This rolls back to commit 47af1fa8e24266d9d631ddb4eaa0139b03a032db.

--- foo/foo.java
+++ foo/foo.java
@@ -1,9 +1,6 @@
 public class foo {
 	
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			System.out.print(i);
-			System.out.println("foo");
-		}
+		System.out.println("foo");
 	}
 }
