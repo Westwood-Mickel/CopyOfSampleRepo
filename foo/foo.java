5f0e1c546bbba36a970d872fa7336d3c01fa0a1a
TX101010 <github@littlebox.net>
2015-03-15 01:01:01 -0500

    Update foo.java

--- foo/foo.java
+++ foo/foo.java
@@ -2,7 +2,7 @@ public class foo {
 	
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			System.out.println(i);
+			System.out.print(i);
 			System.out.println("foo");
 		}
 	}
